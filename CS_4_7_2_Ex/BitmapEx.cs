d68d36111611e333367362b1029a28211bdbc364
holy <holy1017@naver.com>
2020-10-18 16:49:35 +0900

    초기 커밋.

---
+++ CS_4_7_2_Ex/BitmapEx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_4_7_2_Ex
+{
+    class BitmapEx
+    {
+        public void makePng()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(@"TAPSONIC_TOP_");
+            sb.Append(DateTime.Now.ToString("yyyyMMddHHmmss"));
+            sb.Append(@".png");
+            Console.WriteLine(sb.ToString());
+
+            // 1. Create a bitmap
+            using (Bitmap bitmap = new Bitmap(80, 20, PixelFormat.Format24bppRgb))
+            {
+                // 2. Get access to the raw bitmap data
+                BitmapData data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.WriteOnly, bitmap.PixelFormat);
+
+                // 3. Generate RGB noise and write it to the bitmap's buffer.
+                // Note that we are assuming that data.Stride == 3 * data.Width for simplicity/brevity here.
+                byte[] noise = new byte[data.Width * data.Height * 3];
+                new Random().NextBytes(noise);
+                Marshal.Copy(noise, 0, data.Scan0, noise.Length);
+
+                bitmap.UnlockBits(data);
+
+                bitmap.Save(sb.ToString(), ImageFormat.Png);
+
+                // 4. Save as JPEG and convert to Base64
+                using (MemoryStream jpegStream = new MemoryStream())
+                {
+                    bitmap.Save(jpegStream, ImageFormat.Jpeg);
+                    Console.WriteLine(Convert.ToBase64String(jpegStream.ToArray()));
+                }
+            }
+        }
+    }
+}
---
+++ CS_4_7_2_Ex/FileEx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_4_7_2_Ex
+{
+    class FileEx
+    {
+        public void fileMake()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(@"TAPSONIC_TOP_");
+            sb.Append(DateTime.Now.ToString("yyyyMMddHHmmss"));
+            sb.Append(@".txt");
+            Console.WriteLine(sb.ToString());
+
+            using (StreamWriter textWrite = File.CreateText(sb.ToString()))//생성
+            {
+                textWrite.WriteLine("abcdefghijk"); //쓰기
+            }
+        }
+    }
+}
